8895f7769825fe66f91f6d65e59ac9df2a250e01
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-20 17:00:00 +0300

    Add files via upload

---
+++ CW/Day10/Notebook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Intro
+{
+    internal class Notebook : PC
+    {
+        private double _display;
+
+        public double Display
+        {
+            get { return _display; }
+            set { _display = value; }
+        }
+
+        public override void Testing()
+        {
+            base.Testing();
+            Console.WriteLine("Test Display");
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2}", Model, Fp, _display);
+        }
+
+        public override decimal Calculate()
+        {
+            return base.Calculate() * 1.5m;
+        }
+
+        public Notebook(string model, double fp) : base(model, fp) {  }
+    }
+}
---
+++ CW/Day10/PC.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Intro
+{
+    internal class PC : Device
+    {
+        private string _model;
+        private double _fp;
+        protected decimal _price;
+
+        public override string CreateCode()
+        {
+            return "111112222333";
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", _model, _fp);
+        }
+
+        public PC(string model, double fp)
+        {
+            _model = model;
+            _fp = fp;
+            _price = 1000;
+        }
+
+        public virtual decimal Calculate()
+        {
+            return _price;
+        }
+
+        public virtual void Testing()
+        {
+            Console.WriteLine("Test FP");
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            private set { _model = value; }
+        }
+
+        
+        public double Fp
+        {
+            get { return _fp; }
+            private set { _fp = value; }
+        }
+
+    }
+}
