ea6f34fd40e2099ba6d46fd0808c70a9f6ecfafa
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-20 18:26:52 +0300

    Add files via upload

---
+++ CW/Day11/DemoInterface/Interfaces/ISending.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DemoInterface.Interfaces
+{
+    internal interface ISending : IValidator
+    {
+        string ProtocolName { get; }
+        void Send(string fileName);
+    }
+}
