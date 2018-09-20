de4ac62d6fc5d42ffe6bb094010ed0c920da7050
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-20 18:26:38 +0300

    Create IEmailSending.cs

---
+++ CW/Day11/DemoInterface/Interfaces/IEmailSending.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoInterface.Interfaces
+{
+    internal interface IEmailSending
+    {
+        void Send(string message);
+    }
+}
