e59815063cedf4934c813dde145b48102056dd9a
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-20 16:49:17 +0300

    Add files via upload

---
+++ BuildHouse/BuildLibrary/TeamBuilders/TeamLeader.cs
@@ -0,0 +1,43 @@
+﻿using BuildLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildLibrary.TeamBuilders
+{
+    class TeamLeader : IWorker
+    {
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void CheckWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BuildHouse/BuildLibrary/TeamBuilders/Worker.cs
@@ -0,0 +1,64 @@
+﻿using BuildLibrary.Interfaces;
+using System;
+
+namespace BuildLibrary.TeamBuilders
+{
+    internal class Worker : IWorker
+    {
+        #region FILEDS
+        private int _output;
+        private string[] _outInfo;
+        private string _name;
+        private readonly string _position;
+        #endregion
+
+        #region CTORS
+        public Worker(string name)
+        {
+            Name = name;
+            _position = "Builder";
+        }
+        #endregion
+
+        #region IRELIS
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (!BuildExceptions.WorkerException.NameException(value))
+                    throw new ArgumentNullException("Неправильно введены данные");
+                _name = value;
+            }
+        }
+
+        public string Position
+        {
+            get
+            {
+                return _position;
+            }
+        }
+
+        public void CheckWork(IPart obj, int _workRemaining)
+        {
+            _outInfo[_output] = $"{obj.GetType()} {obj.Remained(_workRemaining)} {obj.IsCreated(_workRemaining)}";
+            _output++;
+        }
+        #endregion
+
+        public int Output
+        {
+            get { return _output; }
+            protected set
+            {
+                _output = value;
+                _outInfo = new string[value];
+            }
+        }
+    }
+}
