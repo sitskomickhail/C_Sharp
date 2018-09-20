97eb2db3864b1dd86726f639e6e031a9f32083a6
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-20 16:48:08 +0300

    Add files via upload

---
+++ BuildHouse/BuildLibrary/Interfaces/IWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildLibrary.Interfaces
+{
+    internal interface IWorker
+    {
+        string Name { get; set; }
+        string Position { get; }
+
+        void CheckWork(IPart obj, int _workRemaining);
+    }
+}
