0f7dc8ded69f930ebf4cfb8f27f2d6be865ceb81
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-23 21:51:46 +0300

    Update Walls.cs

--- BuildHouse/BuildLibrary/BuildElems/Walls.cs
+++ BuildHouse/BuildLibrary/BuildElems/Walls.cs
@@ -1,24 +1,36 @@
-﻿using BuildLibrary.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BuildLibrary.Interfaces;
+using BuildLibrary.TeamBuilders;
 
 namespace BuildLibrary.BuildElems
 {
     internal class Walls : IPart
     {
-        private int _height;    
-        public bool IsCreated(int height)
-        {
-            return Int32.Equals(height, this._height);
+        #region FIELDS
+        private int _crHeight;
+        private int _fullHeight;
+        private decimal _pct;
+        #endregion
 
+        #region CTOR
+        public Walls(int height)
+        {
+            _fullHeight = height;
+            _pct = (_crHeight * 100) / _fullHeight;
+        }
+        #endregion
+
+        #region IRELIZ
+        public void AddSlice(Worker builder)
+        {
+            _crHeight++;
+            _pct = (_crHeight * 100) / _fullHeight;
         }
 
-        public int Remained(int value)
-        {
-            throw new NotImplementedException();
-        }
+        public decimal Pct { get { return _pct; } }
+        #endregion
+
+        #region PROP
+        public int CreatedLength { get { return _crHeight; } }
+        #endregion
     }
 }
