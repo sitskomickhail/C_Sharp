5f255977970b89357134db2961da849d8e9eb1cb
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-23 21:51:08 +0300

    Update House.cs

--- BuildHouse/BuildLibrary/BuildElems/House.cs
+++ BuildHouse/BuildLibrary/BuildElems/House.cs
@@ -1,56 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BuildLibrary.TeamBuilders;
+using System;
+using BuildLibrary.Interfaces;
 
 namespace BuildLibrary.BuildElems
 {
     public class House
     {
         #region FIELDS
+        private IPart[] _parts;
         private int _lenght;
         private int _width;
         private int _height;
-        private Basement _base;
-        private Roof _roof;
-        private Walls[] _walls;
-        private Window[] _windows;
-        private Door _door;
+
+        private int _windowCount;
+        private int _doorsCount;
         #endregion
 
-        public House(int lenght, int width, int height, int windowCount)
+        #region CONSTS
+        private const int _baseCount = 1;
+        private const int _roofCount = 1;
+        private const int _wallsCount = 4;
+        #endregion
+
+        #region CTOR
+        public House(int lenght, int height, int width, int windowCount, int doorsCount)
         {
-            _windows = new Window[windowCount];
-            _walls = new Walls[4];
             Lenght = lenght;
-            Width = width;
             Height = height;
-        }
+            Width = width;
 
-        public void StartBuilding(Team _team)
+            if (windowCount < 0 || doorsCount < 0)
+                throw new ArgumentOutOfRangeException("Неверные аргументы");
+
+            _windowCount = windowCount;
+            _doorsCount = doorsCount;
+
+            _parts = new IPart[_baseCount + _roofCount + _wallsCount + windowCount + doorsCount];
+            _parts[0] = new Basement(_lenght);
+            _parts[1] = new Roof(_lenght);
+            for (int i = 2; i < 6; i++)
+            {
+                _parts[i] = new Walls(_height);
+            }
+            int a = 5;
+            for (int i = a; i < 5 + windowCount; i++)
+            {
+                _parts[i] = new Window();
+                a++;
+            }
+            for (int i = a; i < a + doorsCount; i++) { _parts[i] = new Door(); }
+        }
+        #endregion
+
+        #region IDNDEXER
+        public IPart this[int index]
         {
-
+            get { return _parts[index]; }
         }
+        #endregion
 
         #region PROPS
+        public int WinCount { get { return _windowCount; } }
+        public int DoorCount { get { return _doorsCount; } }
+
+        public static int AllCount
+        { get { return _baseCount + _roofCount + _wallsCount; } }
+
         public int Height
         {
             get { return _height; }
-            protected set { _height = value; }
+            protected set
+            {
+                if (!BuildExceptions.Exception.LHWException(value))
+                    throw new ArgumentOutOfRangeException("Null argument"); _height = value;
+            }
         }
 
         public int Width
         {
             get { return _width; }
-            protected set { _width = value; }
+            protected set
+            {
+                if (!BuildExceptions.Exception.LHWException(value))
+                    throw new ArgumentOutOfRangeException("Null argument"); _width = value;
+            }
         }
 
         public int Lenght
         {
             get { return _lenght; }
-            protected set { _lenght = value; }
+            protected set
+            {
+                if (!BuildExceptions.Exception.LHWException(value))
+                    throw new ArgumentOutOfRangeException("Null argument"); _lenght = value;
+            }
         }
         #endregion
     }
