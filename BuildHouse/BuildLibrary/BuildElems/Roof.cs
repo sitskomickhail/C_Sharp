a401acafe39cc8494360ab5b75e977feada5c350
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-23 21:51:24 +0300

    Update Roof.cs

--- BuildHouse/BuildLibrary/BuildElems/Roof.cs
+++ BuildHouse/BuildLibrary/BuildElems/Roof.cs
@@ -1,22 +1,36 @@
-﻿using BuildLibrary.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BuildLibrary.Interfaces;
+using BuildLibrary.TeamBuilders;
 
 namespace BuildLibrary.BuildElems
 {
     internal class Roof : IPart
     {
-        public bool IsCreated(int value)
+        #region FIELDS
+        private int _crLenght;
+        private int _fullLenght;
+        private decimal _pct;
+        #endregion
+
+        #region CTOR
+        public Roof(int lenght)
         {
-            throw new NotImplementedException();
+            _fullLenght = lenght;
+            _pct = (_crLenght * 100) / _fullLenght;
+        }
+        #endregion
+
+        #region IRELIZ
+        public void AddSlice(Worker builder)
+        {
+            _crLenght++;
+            _pct = (_crLenght * 100) / _fullLenght;
         }
 
-        public int Remained(int value)
-        {
-            throw new NotImplementedException();
-        }
+        public decimal Pct { get { return _pct; } }
+        #endregion
+
+        #region PROP
+        public int CreatedLength { get { return _crLenght; } }
+        #endregion
     }
 }
