ec879b88f627197757479ef3bf16fe51eb858a03
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-23 21:52:06 +0300

    Update Window.cs

--- BuildHouse/BuildLibrary/BuildElems/Window.cs
+++ BuildHouse/BuildLibrary/BuildElems/Window.cs
@@ -1,22 +1,32 @@
-﻿using BuildLibrary.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BuildLibrary.Interfaces;
+using BuildLibrary.TeamBuilders;
 
 namespace BuildLibrary.BuildElems
 {
     internal class Window : IPart
     {
-        public bool IsCreated(int value)
+        #region FIELDS
+        private int _crLenght;
+        private int _fullLenght;
+        private decimal _pct;
+        #endregion
+
+        #region CTOR
+        public Window()
         {
-            return Int32.Equals(count, this._count);
+            _fullLenght = 1;
+            _pct = (_crLenght * 100) / _fullLenght;
+        }
+        #endregion
+
+        #region IRELIZ
+        public void AddSlice(Worker builder)
+        {
+            _crLenght++;
+            _pct = (_crLenght * 100) / _fullLenght;
         }
 
-        public int Remained(int value)
-        {
-            throw new NotImplementedException();
-        }
+        public decimal Pct { get { return _pct; } }
+        #endregion
     }
 }
