fbf6898e832211bfd9d6f8b097643475f28653a2
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-23 21:53:59 +0300

    Update TeamLeader.cs

--- BuildHouse/BuildLibrary/TeamBuilders/TeamLeader.cs
+++ BuildHouse/BuildLibrary/TeamBuilders/TeamLeader.cs
@@ -1,24 +1,69 @@
-﻿using BuildLibrary.Interfaces;
+using BuildLibrary.BuildElems;
+using BuildLibrary.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BuildLibrary.TeamBuilders
 {
-    class TeamLeader : IWorker
+    internal class TeamLeader : IWorker
     {
+        #region FIELDS
+        private string _name;
+        private string _position;
+        private string[] _report;
+        #endregion
+
+        #region CTOR
+        public TeamLeader(string name, int workersCount)
+        {
+            Name = name;
+            _position = "Team leader";
+            _report = new string[workersCount * 100];
+        }
+        #endregion
+
+        #region METHODS
+        public void SetWork(IPart obj, Worker[] builders)
+        {
+            builders[Randomer.Next(builders.Length)].AddSlice(obj);
+        }
+
+        public string[] MakeReport(Worker[] builders)
+        {
+            _report[0] = $"         Report made by team leader: {this.Name}\n";
+            int count = 1;
+            for (int i = 0; i < builders.Length; i++)
+            {
+                string[] info = builders[i].OutputInfo;
+                for (int j = 0; j < builders[i].Output; j++)
+                {
+                    _report[count] = info[j];
+                    count++;
+                }
+            }
+            return _report;
+        }
+        #endregion
+
+        #region REALIS
+        public bool CheckWork(IPart obj)
+        {
+            if (obj.Pct != 100)
+                return false;
+            return true;
+        }
+
         public string Name
         {
             get
             {
-                throw new NotImplementedException();
+                return _name;
             }
 
             set
             {
-                throw new NotImplementedException();
+                if (!BuildExceptions.Exception.NameException(value))
+                    throw new ArgumentNullException("Неправильно введены данные");
+                _name = value;
             }
         }
 
@@ -26,18 +71,29 @@ namespace BuildLibrary.TeamBuilders
         {
             get
             {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
+                return _position;
             }
         }
+        #endregion
 
-        public void CheckWork()
+        #region PROPS
+        public string[] GetReport
         {
-            throw new NotImplementedException();
+            get
+            {
+                int count = 0;
+                while (_report[count] != null)
+                {
+                    count++;
+                }
+                string[] outFunc = new string[count];
+                for (int i = 0; i < count; i++)
+                {
+                    outFunc[i] = _report[i];
+                }
+                return outFunc;
+            }
         }
+        #endregion
     }
 }
