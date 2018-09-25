f53c5cde813cdc7880c356e42e3125911e3a0b1e
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-09-25 16:33:38 +0300

    Add files via upload

---
+++ Racing/RaceLibrary/Track.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceLibrary
+{
+    class Track : Automobile
+    {
+        public override double Patency
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override double TopSpeed
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override double Weight
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override double EnlSpeed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
