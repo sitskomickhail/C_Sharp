adaf26a8c853c5f3713f7a6e48042c75dd7952a0
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-10-04 20:05:33 +0300

    Add files via upload

---
+++ CW/Day20/6Tasks/EmployeeInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLinqLib
+{
+	public class EmployeeInfo
+	{
+		public int Id { get; set; }
+		public int ParentId { get; set; }
+		public string Name { get; set; }
+		public string Position { get; set; }
+		public int DepartmentId { get; set; }
+		public decimal Salary { get; set;  }
+	}
+}
