7ea297029773adac4c9c82a340d9335c541981a7
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2018-10-10 21:18:55 +0300

    Add files via upload

---
+++ AiroplaneSimulator/PlaneLib/Dispatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PilotLib
+{
+    public class Dispatcher
+    {
+        private int _wCorrect = Randomer.Next(-200, 200);
+        private int _erPoints;
+        private int _height;
+
+        public int ErrorPoints
+        {
+            get { return _erPoints; }
+            set { _erPoints = value; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+        }
+
+        public void CalcHeight(Airoplane plane)
+        {
+            _height = 7 * plane.Speed - _wCorrect;
+            if (_height < 0)
+                _height = 0;
+        }
+
+
+    }
+}
---
+++ AiroplaneSimulator/PlaneLib/Randomer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PilotLib
+{
+    public static class Randomer
+    {
+        private static Random rand;
+        static Randomer()
+        {
+            rand = new Random();
+        }
+
+        public static int Next()
+        {
+            return rand.Next();
+        }
+
+        public static int Next(int num1)
+        {
+            return rand.Next(num1);
+        }
+
+        public static int Next(int num1, int num2)
+        {
+            return rand.Next(num1, num2);
+        }
+    }
+}
